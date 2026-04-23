88fdc26014a25e508eb7b9d53c9e558a37397845
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2026-04-24 01:21:43 +0200

    feat: WIP add basic user app
    	- start new app users
    	- add new custom model user
    	- add migration file
    	- register model on the admin dashboard

--- fishing_store/users/admin.py
+++ fishing_store/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "role", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (("Додаткова інформація", {"fields": ("role",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- fishing_store/users/models.py
+++ fishing_store/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Кастомна модель користувача для рибацького магазину
+
+    Може бути або користувачем, або покупцем
+    """
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "адміністратор"
+        CUSTOMER = "CUSTOMER", "покупець"
+
+    role = models.CharField(
+        max_length=15,
+        choices=Role.choices,
+        default=Role.CUSTOMER,
+        verbose_name="Роль користувача",
+    )
+
+    def is_manager(self):
+        """
+        Перевірка на наявність дозволів адміна
+        """
+        return self.role == self.Role.ADMIN or self.is_superuser
