c40a95f1cc1b52da8597702cb8e6fe07e5cb1712
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2026-05-14 22:06:05 +0200

    fix: update debug value logic

--- fishing_store/fishing_store/settings.py
+++ fishing_store/fishing_store/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "t")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
